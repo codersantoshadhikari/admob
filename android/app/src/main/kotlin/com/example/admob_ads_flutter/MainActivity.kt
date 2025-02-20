3173622b4d9a6e62fb82a6dcca014f61c10df7da
smaittech <smaittech@MacBookAir.Home>
2025-02-20 13:10:54 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/admob_ads_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.admob_ads_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:admob_ads_flutter/banner_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: BannerScreen());
+  }
+}
